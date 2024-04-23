965f50334b6340af4b1c04c107d761167da5e712
yacine20005 <yacine200005@gmail.com>
2024-04-23 09:31:18 +0200

    Ajout des fonctions permettant la génération des grilles et lecture des fichiers .txt

--- cropta.py
+++ cropta.py
@@ -1,8 +1,38 @@
-def creer_grille(n):
+#R = route
+#H = herbe
+#A = arrivée
+#D = départ
+
+def creer_grille(x, y):
     grille = []
-    for x in range(n):
+    for _ in range(y):
         ligne = []
-        for y in range(n):
-            ligne.append(0)
+        for _ in range(x):
+            ligne.append("R")
         grille.append(ligne)
     return grille
+
+
+def afficher_grille(grille):
+    for ligne in grille:
+        print(ligne)
+
+
+def convertion(fichier):
+    with open(f"maps-texte/{fichier}") as fichier:
+        readlines = [ligne.rstrip() for ligne in fichier.readlines()]
+        x = len(readlines)
+        y = len(readlines[0])
+        grille = creer_grille(y, x)
+        afficher_grille(grille)
+        for y in range(y):
+            for x in range(x):
+                if readlines[y][x] == "#":
+                    grille[y][x] = "H"
+                if readlines[y][x] == ">":
+                    grille[y][x] = "A"
+                if readlines[y][x] == "*":
+                    grille[y][x] = "D"
+
+
+convertion("map_mini.txt")
