eb78eaf9e6e5a68b0974347d9e75e5dde38dafb5
yacine20005 <yacine200005@gmail.com>
2024-05-05 23:39:17 +0200

    Refactorisation du code pour une meilleure efficacité et tentatives pour le solveur

--- moteur.py
+++ moteur.py
@@ -136,20 +136,21 @@ def gerer_evenement(ev,plateau_pion, mvtpossible, poseactuelle, pos_parcouru):
         pos_parcouru.append([Xcaseclick, Ycaseclick])
 
 def coup_possible(plateau, pospion, posacheck, regle):
-    hauteur_case = fltk.hauteur_fenetre() / len(plateau) 
-    largeur_case = fltk.largeur_fenetre() / len(plateau[0])
     if (0 <= posacheck[1] < len(plateau) and 0 <= posacheck[0] < len(plateau[0]) 
                 and plateau[posacheck[1]][posacheck[0]] != "H"):
             if regle == "souple":
                 return True
-            for y in range(len(plateau)):
-                for x in range(len(plateau[y])):
-                    if plateau[y][x] == "H":
-                        if intersection_cercle(pospion[0] * largeur_case, pospion[1] * hauteur_case,
-                                            posacheck[0] * largeur_case, posacheck[1] * hauteur_case,
-                                            x * largeur_case, y * hauteur_case, hauteur_case * 0.75):
-                            return False
-            return True
+            else:
+                hauteur_case = fltk.hauteur_fenetre() / len(plateau) 
+                largeur_case = fltk.largeur_fenetre() / len(plateau[0])
+                for y in range(len(plateau)):
+                    for x in range(len(plateau[y])):
+                        if plateau[y][x] == "H":
+                            if intersection_cercle(pospion[0] * largeur_case, pospion[1] * hauteur_case,
+                                                posacheck[0] * largeur_case, posacheck[1] * hauteur_case,
+                                                x * largeur_case, y * hauteur_case, hauteur_case * 0.75):
+                                return False
+                return True
     return False
 
 def distance_centre(x1, y1, x2, y2, xc, yc):
--- solveur.py
+++ solveur.py
@@ -1,48 +1,48 @@
 import collections
 import moteur
-
-
-def solveur_old(grille, pos_depart, map):
-    deque = collections.deque()
-    deque.append((pos_depart, [pos_depart],
-                  moteur.list_add2(pos_depart, pos_depart)))
-    visite = []
-    #set
-    while deque != []:
-        print(deque)
-        (pos, chemin, vitesse) = deque.pop()
-        mvt_possible = moteur.calcul_posibilite(grille, pos, chemin)
-        if not moteur.defaite(mvt_possible):
-            for next_pos in mvt_possible:
-                if (next_pos, vitesse) in visite:
-                    print("Lavabo")
-                elif (next_pos, vitesse) not in visite:
-                    if moteur.victoire(chemin + [next_pos], grille):
-                        moteur.sauvegarde_partie(chemin + [next_pos], map, 9)
-                        return chemin + [next_pos]
-                    deque.append((next_pos, chemin + [next_pos], moteur.list_add2(next_pos, pos)))
-                    visite.append((next_pos, moteur.list_add2(next_pos, pos)))
-    return None
+import affichage
+import fltk
+import time
+import sys
 
 def solveur_profondeur(grille, chemin, visite, map):
     print('sku')
     if len(chemin) < 2:
         vitesse = (1, 1)
     else:
-        vitesse = moteur.list_add2(chemin[-2], chemin[-1])
-    mvt_possible = moteur.calcul_posibilite(grille, chemin[-1], chemin)
+        vitesse = affichage.list_add2(chemin[-2], chemin[-1])
+        vitesse = (abs(vitesse[0]), abs(vitesse[1]))
+    mvt_possible = moteur.calcul_posibilite(grille, chemin[-1], chemin, "souple")
     for next_pos in mvt_possible:
-        if (tuple(next_pos), tuple(vitesse)) in visite:
-            return None
-        elif (tuple(next_pos), tuple(vitesse)) not in visite:
+        if (tuple(next_pos), tuple(vitesse)) not in visite:
             if moteur.victoire(chemin + [next_pos], grille):
-                moteur.sauvegarde_partie(chemin + [next_pos], map, 9)
+                moteur.sauvegarde_partie(chemin + [next_pos], map, 9, "souple")
                 return chemin + [next_pos]
-            visite.add((tuple(next_pos), tuple(moteur.list_add2(next_pos, chemin[-1]))))
-            return solveur_profondeur(grille, chemin + [next_pos], visite, map)
+            visite.add((tuple(next_pos), tuple(affichage.list_add2(next_pos, chemin[-1]))))
+            result = solveur_profondeur(grille, chemin + [next_pos], visite, map)
+            if result is not None:
+                return result
+    return None
 
+def recherche_profondeur_iteratif(grille, chemin_initial, visite, map):
+    pile = [(chemin_initial, (1, 1))]
+    while pile:
+        chemin, vitesse = pile.pop()
+        vitesse = (abs(vitesse[0]), abs(vitesse[1]))  # Modify vitesse to absolute value
+        mvt_possible = moteur.calcul_posibilite(grille, chemin[-1], chemin, "souple")
+
+        for next_pos in mvt_possible:
+            if (tuple(next_pos), tuple(vitesse)) not in visite:
+                if moteur.victoire(chemin + [next_pos], grille):
+                    moteur.sauvegarde_partie(chemin + [next_pos], map, 9, "souple")
+                    return True
+                visite.add((tuple(next_pos), tuple(affichage.list_add2(next_pos, chemin[-1]))))
+                pile.append((chemin + [next_pos], affichage.list_add2(next_pos, chemin[-1])))
+    return False
+
+sys.setrecursionlimit(100000)
 visite = set()
-map="map1.txt"
+map="map_test.txt"
 grille=moteur.conversion_txt(map)
 pion, pos_depart=moteur.miseenplacepion(grille)
-solveur_profondeur(grille, [pos_depart], visite, map)
+print(recherche_profondeur_iteratif(grille, [pos_depart], visite, map))
